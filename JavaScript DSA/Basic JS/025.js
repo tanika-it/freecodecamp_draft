f49bad2f328b1cc17739d03c1a71661f6ea1bfb6
Tatiana Skorokhodova <48140927+tiana-code@users.noreply.github.com>
2024-07-25 05:56:20 +0300

    Create 025.js

---
+++ JavaScript DSA/Basic JS/025.js
@@ -0,0 +1,3 @@
+const myStr = "I am a \"double quoted\" string inside \"double quotes\"."; 
+
+// This signals to JavaScript that the following quote is not the end of the string, but should instead appear inside the string.
